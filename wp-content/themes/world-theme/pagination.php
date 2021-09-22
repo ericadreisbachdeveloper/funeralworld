55de7bf62c3e0e0b7410e3c68a6137570c424597
erica dreisbach <erica@ericadreisbach.com>
2021-09-22 13:11:56 -0500

    deleting wp-content

--- wp-content/themes/world-theme/pagination.php
+++
@@ -1,7 +0,0 @@
-<?php if ( ! defined( 'ABSPATH' ) ) {  exit; } ?>
-
-
-
-<div class="pagination -archive">
-	<?php dbllc_pagination(); ?>
-</div>
--- wp-content/themes/world-theme/template/editbutton.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined( 'ABSPATH' ) ) {  exit; } ?>
-
-
-
-<?php if(is_user_logged_in()) : ?>
-  <div class="clear"> </div>
-  <?php wp_reset_query(); edit_post_link(); ?>
-  <div class="clear"> </div>
-<?php endif; ?>
