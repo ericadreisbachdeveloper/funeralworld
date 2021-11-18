caa22dcadbcbebff2777b217db30343d58a47842
erica dreisbach <erica@ericadreisbach.com>
2021-11-18 09:01:54 -0600

    updates to search results container

--- files/themes/world-theme/search.php
+++ files/themes/world-theme/search.php
@@ -64,55 +64,58 @@
 									<div class="gutenberg-section core-shortcode">
 										<div class="gutenberg-container">
 											<div id="search-results">
+												<div class="container">
 
-											<?php $s = sprintf( __('%s', 'dbllc'), $wp_query->found_posts);
-											if($s == '1') { $sp = ''; } else { $sp = 's'; } ?>
+												<?php $s = sprintf( __('%s', 'dbllc'), $wp_query->found_posts);
+												if($s == '1') { $sp = ''; } else { $sp = 's'; } ?>
 
-											<?php $str = get_search_query();
-												    $for_query = ''; if($str != '') /* { $for_query = ' for &ldquo;' . $str . '&rdquo;'; }*/ ?>
+												<?php $str = get_search_query();
+													    $for_query = ''; if($str != '') /* { $for_query = ' for &ldquo;' . $str . '&rdquo;'; }*/ ?>
 
-											<h1 class="search-results-h1<?php if(!array_filter($search_array)) : ?> -no-filters<?php endif; ?>"><?php _e(sprintf( __( '%s Search Result' . $sp . $for_query, 'dbllc' ), $wp_query->found_posts ));  ?></h1>
+												<h1 class="search-results-h1<?php if(!array_filter($search_array)) : ?> -no-filters<?php endif; ?>"><?php _e(sprintf( __( '%s Search Result' . $sp . $for_query, 'dbllc' ), $wp_query->found_posts ));  ?></h1>
 
 
-											<?php if(array_filter($search_array)) : ?>
+												<?php if(array_filter($search_array)) : ?>
 
-											<!-- all values are empty (where "empty" means == false) -->
-											<div class="filter-buttons">
+												<!-- all values are empty (where "empty" means == false) -->
+												<div class="filter-buttons">
 
-												<?php if($search_array[0] != '') : ?>
-												<a href="#" data-input="s" data-value="<?= $search_array[0]; ?>">SEARCH: <?= $search_array[0]; ?></a>
+													<?php if($search_array[0] != '') : ?>
+													<a href="#" data-input="s" data-value="<?= $search_array[0]; ?>">SEARCH: <?= $search_array[0]; ?></a>
+													<?php endif; ?>
+
+													<?php if($search_array[1] != '') : ?>
+													<?php $audience_term = get_term_by('slug', $search_array[1], 'audience'); ?>
+													<a href="#" data-input="audience" data-value="<?= $audience_term->slug; ?>">AUDIENCE: <?= $audience_term->name; ?></a>
+													<?php endif; ?>
+
+													<?php if($search_array[2] != '') : ?>
+													<?php $author_term = get_user_by('id', $search_array[2]); ?>
+													<a href="#" data-input="author" data-value="<?= $author_term->ID; ?>">AUTHOR: <?= $author_term->display_name; ?></a>
+													<?php endif; ?>
+
+													<?php if($search_array[3] != '') : ?>
+													<?php $topic_term = get_term_by('slug', $search_array[3], 'topic'); ?>
+													<a href="#" data-input="topic" data-value="<?= $topic_term->slug; ?>">TOPIC: <?= $topic_term->name; ?></a>
+													<?php endif; ?>
+
+													<?php if($search_array[4] != '') : ?>
+													<?php $type_term = get_term_by('slug', $search_array[4], 'resource-type'); ?>
+													<a href="#" data-input="resource-type" data-value="<?= $type_term->slug; ?>">RESOURCE TYPE: <?= $type_term->name; ?></a>
+													<?php endif; ?>
+
+												</div><!-- /.filter-buttons -->
 												<?php endif; ?>
 
-												<?php if($search_array[1] != '') : ?>
-												<?php $audience_term = get_term_by('slug', $search_array[1], 'audience'); ?>
-												<a href="#" data-input="audience" data-value="<?= $audience_term->slug; ?>">AUDIENCE: <?= $audience_term->name; ?></a>
-												<?php endif; ?>
-
-												<?php if($search_array[2] != '') : ?>
-												<?php $author_term = get_user_by('id', $search_array[2]); ?>
-												<a href="#" data-input="author" data-value="<?= $author_term->ID; ?>">AUTHOR: <?= $author_term->display_name; ?></a>
-												<?php endif; ?>
-
-												<?php if($search_array[3] != '') : ?>
-												<?php $topic_term = get_term_by('slug', $search_array[3], 'topic'); ?>
-												<a href="#" data-input="topic" data-value="<?= $topic_term->slug; ?>">TOPIC: <?= $topic_term->name; ?></a>
-												<?php endif; ?>
-
-												<?php if($search_array[4] != '') : ?>
-												<?php $type_term = get_term_by('slug', $search_array[4], 'resource-type'); ?>
-												<a href="#" data-input="resource-type" data-value="<?= $type_term->slug; ?>">RESOURCE TYPE: <?= $type_term->name; ?></a>
-												<?php endif; ?>
-
-											</div><!-- /.filter-buttons -->
-											<?php endif; ?>
+												</div><!-- /.container -->
 
 
 											<?php get_template_part('loop-search'); ?>
 
 
 											<?php get_template_part('pagination'); ?>
-											</div><!-- /#search-results -->
 
+										</div><!-- /#search-results -->
 
 							</div><!-- /.gutenberg-container -->
 						</div><!-- /.gutenberg-section.core-shortcode -->
