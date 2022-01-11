eddb3972e663aa9c449bd0b562e977ec10420515
erica dreisbach <erica@ericadreisbach.com>
2022-01-11 14:02:23 -0600

    update to author archive page, hide deck if empty

--- files/themes/world-theme/author.php
+++ files/themes/world-theme/author.php
@@ -9,7 +9,7 @@
 	<div class="container">
 		<h1>Author: <?php $obj = get_queried_object(); print_r($obj->display_name); ?></h1>
 
-		<?php $author_id = $obj->ID; $author_meta = get_user_meta($author_id); if($author_meta['description'] != '') : ?>
+		<?php $author_id = $obj->ID; $author_meta = get_user_meta($author_id); if($author_meta['description'][0] !== '') : ?>
 		<p class="deck"><?= $author_meta['description'][0]; ?> </p>
 		<?php endif; ?>
 
